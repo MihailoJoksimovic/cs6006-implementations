80f14f847e630142d27996f5d3f5328c8566a6c2
Mihailo Joksimovic <mihailo.joksimovic@pool4tool.com>
2020-01-23 21:49:36 +0100

    WIP: Done with appending

--- binary_search.py
+++ binary_search.py
@@ -11,9 +11,23 @@ def binary_search(array, element, low, high):
     middle = floor((low + high) / 2)
 
     if element > array[middle]:
-        return binary_search(array, element, middle + 1, len(array) - middle)
+        return binary_search(array, element, middle + 1, high)
     else:
         return binary_search(array, element, low, middle - 1)
+
+def insertion_sort(array, element):
+    insert_idx = binary_search(array, element, 0, len(array) - 1)
+
+    # Just append last element to the end so that elements are properly shifted
+    array.append(array[len(array) - 1])
+
+    for i in range(len(array) - 1, insert_idx, -1):
+        array[i] = array[i - 1]
+
+    array[insert_idx] = element
+
+    return array
+
 
 # total_iterations = 0
 
@@ -23,4 +37,7 @@ arr = [0, 4, 8, 10, 12, 18]
 
 arr = [5, 10, 20, 30]
 arr = [5, 10, 20, 30, 50]
-print(binary_search(arr, 40, 0, len(arr) - 1))
+# arr = [5, 10]
+print(binary_search(arr, 70, 0, len(arr) - 1))
+
+print(insertion_sort(arr, 25))
--- insertion_sort.py
+++ insertion_sort.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 def _should_swap(left, right, reverse = False):
     """
     Returns true if left and right elements need to be swapped
@@ -28,11 +30,52 @@ def insertion_sort(array, reverse = False):
 
     return array
 
+def find_swap_index(array, key):
+    """Finds an index where the swap needs to happen"""
+
+    idx = None
+
+    if len(array) == 1 and array[0] != key:
+        return None
+
+    # Find middle element
+    if len(array) % 2 == 0:
+        middleIdx = int(len(array) / 2)
+    else:
+        middleIdx = int((len(array) / 2) + 0.5)
+
+    middleElement = array[middleIdx]
+
+    if middleElement == key:
+        return middleIdx
+
+    if len(array) == 1:
+        return None
+
+    if middleElement > key:
+        # We need to look on the LEFT side
+        subArray = array[0:middleIdx]
+    else:
+        subArray = array[middleIdx:]
+
+    return find_swap_index(subArray, key)
+
+def binary_insertion_sort(array):
+    """Uses binary search to find the element to sort by ..."""
+
+    # We're starting from first element ...
+    # for i in range(1, len(array)):
+
+
 arr = [37, 23, 0, 17, 12, 72, 31, 46, 100, 88, 54]
 expected = [0, 12, 17, 23, 31, 37, 46, 54, 72, 88, 100]
 
-print(insertion_sort(arr, True))
+# print(insertion_sort(arr, True))
+#
+# print(insertion_sort(arr))
+#
+# print("Assertion went through! Good job!")
 
-print(insertion_sort(arr))
+# print(find_swap_index([0, 1, 2, 3, 4], 100))
 
-print("Assertion went through! Good job!")
+print(find_swap_index([0, 100], 100))
