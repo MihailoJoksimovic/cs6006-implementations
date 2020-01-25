f6dab27e0d88603d0142a9fe4842f59f388fae2e
Mihailo Joksimovic <mihailo.joksimovic@pool4tool.com>
2020-01-25 10:57:42 +0100

    WIP: Finish basic binary search

--- practice/insertion_sort.py
+++ practice/insertion_sort.py
@@ -1,6 +1,27 @@
 # 1. Implement insertion sort
 # 2. Implement with binary search
 
+from math import floor
+
+def binary_search(array, element, start, end):
+    """Find index of given element in array"""
+
+    middle = floor((start + end) / 2)
+
+    if array[middle] == element:
+        return middle
+
+    # If only one element's left --> compare to it and return the appropriate position
+    if (end - start) == 1:
+        return start if array[middle] > element else start + 1
+
+    if array[middle] > element:
+        # Element is to the left
+        return binary_search(array, element, start, middle - 1)
+    else:
+        # Element is to the right
+        return binary_search(array, element, middle + 1, end)
+
 def insertion_sort(array):
 
     for i in range(1, len(array)):
@@ -27,6 +48,9 @@ def insertion_sort(array):
 arr = [37, 23, 0, 17, 12, 72, 31, 46, 100, 88, 54]
 expected = [0, 12, 17, 23, 31, 37, 46, 54, 72, 88, 100]
 
-insertion_sort(arr)
+# insertion_sort(arr)
+
+input = [5, 10, 20, 40]
+print(binary_search(input, 50, 0, len(input)))
 
 # assert(insertion_sort(arr) == expected)
