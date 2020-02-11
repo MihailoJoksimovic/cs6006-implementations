b39fbe86cd1c9c3d7ca9a40168ea7236b8e78a58
Mihailo Joksimovic <mihailo.joksimovic@pool4tool.com>
2020-02-11 08:12:30 +0100

    WIP: AVL treest -- won't finish

--- avl_trees.py
+++ avl_trees.py
@@ -23,23 +23,95 @@ class Node:
         return self.right.height() + 1 if self.right else 0
 
     def insert(self, value):
+        print("Inserting {}".format(value))
+
+        new_node = None
+
         if value == self.value:
             return
 
         if value < self.value:
             if self.left is None:
                 self.left = Node(value, self)
+
+                new_node = self.left
             else:
-                self.left.insert(value)
+                new_node = self.left.insert(value)
 
         if value > self.value:
             if self.right is None:
                 self.right = Node(value, self)
+
+                new_node = self.right
             else:
-                self.right.insert(value)
+                new_node = self.right.insert(value)
+
+        if self.parent:
+            walk_up_and_check_for_disbalance(self.parent)
+
+        return new_node
+
+    def left_rotate(self):
+        """
+        Does a left rotation on the node
+        Left-rotation takes the RIGHT CHILD (y), places it as a PARENT of current node,
+        and takes y's LEFT child and makes it x's right child.
+        """
+
+        y = self.right
+
+        y.parent = self.parent
+
+        self.parent = y
+
+        self.right = y.left
+
+        y.left = self
+
+    def right_rotate(self):
+        """
+        Does a right rotation on the node.
+
+        Right-rotation takes the LEFT child of the node (x), makes it parent of current node,
+        takes x's RIGHT CHILD and makes it y's left child
+        :return:
+        """
+
+        x = self.left
+
+        x.parent = self.parent
+
+        if self.parent.left == self:
+            self.parent.left = x
+        else:
+            self.parent.right = x
+
+        self.left = x.right
+
+        self.parent = x
+
+        x.right = self
+
+
+
+
+
+
+def walk_up_and_check_for_disbalance(node: Node):
+    if abs(node.left_height() - node.right_height()) > 1:
+        print("Node: {} needs rebalancing! Left: {}, Right: {}".format(node.value, node.left_height(), node.right_height()))
 
 def visit(node: Node):
-    print("Value: {}, Left: {}, Right: {}".format(node.value, node.left_height(), node.right_height()))
+    print(
+        "Value: {}, Parent: {}, Left height: {}, Left child: {}, Right height: {}, Right child: {}".format(
+            node.value,
+            node.parent.value if node.parent else "-",
+            node.left_height(),
+            node.left.value if node.left else "-",
+            node.right_height(),
+            node.right.value if node.right else "-"
+        ),
+    )
 
 
 def print_binary_search_tree(root: Node):
@@ -61,15 +133,22 @@ def print_binary_search_tree(root: Node):
         if node.right is not None:
             queue.append(node.right)
 
-root = Node(50)
+root = Node(41)
 
-root.insert(40)
-root.insert(60)
-root.insert(45)
-root.insert(35)
-
+root.insert(20)
 root.insert(65)
-root.insert(66)
-root.insert(55)
+root.insert(11)
+node_29 = root.insert(29)
+
+root.insert(50)
+root.insert(26)
+root.insert(23)
+
+
+print_binary_search_tree(root)
+
+node_29.right_rotate()
+
+print("After right rotation: ")
 
 print_binary_search_tree(root)
